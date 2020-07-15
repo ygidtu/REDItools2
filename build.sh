0f95eebd7758571f519fb0dfeec2e7d2977831a1
ygidtu <ygidtu@gmail.com>
2020-07-15 11:48:12 +0800

    update build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-env GOOS=darwin GOARCH=amd64 go build -o REDItools2_darwin_amd64 .
-env GOOS=linux GOARCH=amd64 go build -o REDItools2_linux_amd64 .
-env GOOS=windows GOARCH=amd64 go build -o REDItools2_win_amd64.exe .
+git checkout master
+env GOOS=darwin GOARCH=amd64 go build -o REDItools2_l_darwin_amd64 .
+env GOOS=linux GOARCH=amd64 go build -o REDItools2_l_linux_amd64 .
+env GOOS=windows GOARCH=amd64 go build -o REDItools2_l_win_amd64.exe .
 
+
+git checkout dev
+env GOOS=darwin GOARCH=amd64 go build -o REDItools2_h_darwin_amd64 .
+env GOOS=linux GOARCH=amd64 go build -o REDItools2_h_linux_amd64 .
+env GOOS=windows GOARCH=amd64 go build -o REDItools2_h_win_amd64.exe .
+
+git checkout master
